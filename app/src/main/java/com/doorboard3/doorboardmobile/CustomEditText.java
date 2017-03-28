572186b0c53e3f096822c57b791f23d8154d29f6
Daniel Chen <chen.daniel7@gmail.com>
2017-03-28 11:11:13 -0400

    Fixed message layout and CustomEditText

--- app/src/main/java/com/doorboard3/doorboardmobile/CustomEditText.java
+++ app/src/main/java/com/doorboard3/doorboardmobile/CustomEditText.java
@@ -1,13 +1,13 @@
 package com.doorboard3.doorboardmobile;
 
 import android.content.Context;
+import android.support.v7.widget.AppCompatEditText;
 import android.util.AttributeSet;
-import android.widget.EditText;
 
 /**
  * Created by danielchen on 3/25/17.
  */
-public class CustomEditText extends EditText {
+public class CustomEditText extends AppCompatEditText {
 
     public CustomEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
